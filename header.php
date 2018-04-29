5e3f061c6a7598b5134139f1085fb203e520adc2
mehul0810 <mehul.gohil0810@gmail.com>
2018-04-29 20:25:40 +0530

    Initial Version

---
+++ header.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package MeGo
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'mego' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+
+		<?php get_template_part( 'components/header/sliding', 'panel' ); ?>
+
+		<?php get_template_part( 'components/header/header', 'fullscreen-image' ); ?>
+
+		<?php mego_the_custom_logo(); ?>
+
+	</header>
+	<div id="content" class="site-content">
